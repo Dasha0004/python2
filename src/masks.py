0b59f07288403d49abd748ba37ab0fdff435b4cc
darya <aa10111982@inbox.ru>
2025-09-01 14:22:27 +0400

    init

--- src/masks.py
+++ src/masks.py
@@ -13,12 +13,14 @@ def get_mask_card_number(card_number: str) -> str:
     part5 = clean_number[-4:]
     return f"{part1} {part2}{part3} {part4} {part5}"
 
+
 def get_mask_account(account_number: str) -> str:
     """Маскирует номер банковского счета, показывая только последние 4 цифры"""
     clean_number = "".join(filter(str.isdigit, account_number))
     if len(clean_number) <= 4:
         return "Неверный номер счета"
     return f"**{clean_number[-4:]}"
+
 
 print(get_mask_card_number("7000792289606361"))
 print(get_mask_account("73654108430135874305"))
--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,31 @@
-def mask_account_card(info: str) -> str :
+
+from masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(info: str) -> str:
+    """
+    Принимает строку с типом и номером карты или счета, например:
+    "Visa Platinum 7000792289606361", "Maestro 7000792289606361", "Счет 73654108430135874305"
+    Возвращает строку с замаскированным номером, используя разные маски для карт и счетов.
+    """
     parts = info.split()
     number = parts[-1]
+
     card_type = " ".join(parts[:-1])
 
     if card_type.lower().startswith("счет"):
-        masked_number = mask_account(number)
-        else:
-        masked_number = mask_card(number)
-        return f"{card_type} {masked_number}"
+        masked_number = get_mask_account(number)
+    else:
+        masked_number = get_mask_card_number(number)
+
+    return f"{card_type} {masked_number}"
 
 
-
-
-
+def get_date(date_str: str) -> str:
+    """
+    Принимает строку с датой в формате "2024-03-11T02:26:18.671407"
+    Возвращает строку с датой в формате "ДД.ММ.ГГГГ" (например, "11.03.2024").
+    """
+    date_part = date_str.split("T")[0]
+    year, month, day = date_part.split("-")
+    return f"{day}.{month}.{year}"
